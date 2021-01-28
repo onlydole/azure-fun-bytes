763dbcf6d72754fb7f660de64266d07ea036bc08
Kerim Satirli <k.satirli@gmail.com>
2021-01-28 14:07:15 +0100

    adds comments

--- secret.tf
+++ secret.tf
@@ -1,3 +1,4 @@
+# see https://registry.terraform.io/providers/hashicorp/kubernetes/2.0.1
 provider "kubernetes" {
   host                   = module.aks.host
   client_certificate     = base64decode(module.aks.client_certificate)
@@ -5,13 +6,14 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
 }
 
+# see https://registry.terraform.io/providers/hashicorp/kubernetes/2.0.1/docs/resources/secret
 resource "kubernetes_secret" "consul_gossip_encryption_key" {
   metadata {
     name = "consul-gossip-encryption-key"
   }
 
-  # You'll want to use your own secret or something like HashiCorp Vault to generate this regularly... 
-  # but alas, this is only a demo
+  # We are hard-coding this value for the purpose of this demo. This is NOT a best-practice!
+  # For secrets management, we recommend HashiCorp Vault or your Cloud provider's service(s).
   data = {
     key = "VuM/Fj4rf+QFUD8jo4Hg7k2JEKMjAMFHOePPXTkXqxE="
   }
