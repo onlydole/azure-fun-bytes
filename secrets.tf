2b542434c56209fec635521a4a50a8e8c161a246
Kerim Satirli <k.satirli@gmail.com>
2021-01-28 14:38:04 +0100

    updates file locations

--- secrets.tf
+++ secrets.tf
@@ -1,11 +1,3 @@
-# see https://registry.terraform.io/providers/hashicorp/kubernetes/2.0.1
-provider "kubernetes" {
-  host                   = module.aks.host
-  client_certificate     = base64decode(module.aks.client_certificate)
-  client_key             = base64decode(module.aks.client_key)
-  cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
-}
-
 # see https://registry.terraform.io/providers/hashicorp/kubernetes/2.0.1/docs/resources/secret
 resource "kubernetes_secret" "consul_gossip_encryption_key" {
   metadata {
